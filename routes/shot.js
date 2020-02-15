bbdc376713f4d97d524ed44eaff3f473e56ed608
max <maxim181011@mail.ru>
2020-02-15 14:40:47 +0300

    snapshot url from query params

--- routes/shot.js
+++ routes/shot.js
@@ -4,15 +4,17 @@ var puppeteer = require('puppeteer');
 const router = express.Router();
 
 router.get('/', async function(req, res, next) {
-    puppyShot();
+    console.log(req.query.url);
+
+    puppyShot(req.query.url);
 
     res.render('shot', { title: 'Express' });
 });
 
-const puppyShot = async () => {
+const puppyShot = async (url) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
+    await page.goto(url, {waitUntil: 'networkidle2'});
     await page.emulateMedia('screen');
     await page.pdf({path: 'page.pdf', format: 'A4'});
 
